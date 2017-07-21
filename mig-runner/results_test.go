be8be22d04d1802e613f72b049e1414077d6da62
Aaron Meihm <ameihm@mozilla.com>
2017-07-20 22:32:32 -0500

    mig-runner: add test for incoming result processing

--- mig-runner/results_test.go
+++ mig-runner/results_test.go
@@ -6,11 +6,114 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/spf13/afero"
+	"io/ioutil"
+	"mig.ninja/mig"
 	"path"
 	"testing"
 	"time"
 )
+
+var testRunnerResult = `
+{
+    "action": {
+        "counters": {},
+        "description": {},
+        "expireafter": "2017-07-10T22:36:00.018410385Z",
+        "finishtime": "9998-01-11T11:11:11.000000011Z",
+        "id": 8652929898678,
+        "lastupdatetime": "0009-01-11T11:11:11.000000011Z",
+        "name": "mig-runner: test",
+        "operations": [
+            {
+                "module": "file",
+                "parameters": {
+                    "searches": {
+                        "s1": {
+                            "contents": [
+                                "testing"
+                            ],
+                            "names": [
+                                "."
+                            ],
+                            "options": {
+                                "macroal": false,
+                                "matchall": true,
+                                "matchlimit": 1000,
+                                "maxdepth": 1000,
+                                "maxerrors": 30,
+                                "mismatch": null
+                            },
+                            "paths": [
+                                "/home"
+                            ],
+                            "sizes": [
+                                "<1m"
+                            ]
+                        }
+                    }
+                }
+            }
+        ],
+        "pgpsignatures": [
+            "TESTINGSIGNATURE"
+        ],
+        "starttime": "0009-01-11T11:11:11.000000011Z",
+        "status": "pending",
+        "syntaxversion": 2,
+        "target": "(tags#>>'{class}') is null",
+        "threat": {},
+        "validfrom": "2017-07-10T22:34:00.018410385Z"
+    },
+    "commands": null,
+    "name": "test",
+    "plugin": "testplugin"
+}
+`
+
+func TestIncomingResult(t *testing.T) {
+	var (
+		err error
+		res mig.RunnerResult
+	)
+	ctx, err = initContext("./testdata/runner.cfg")
+	if err != nil {
+		t.Fatalf("initContext: %v", err)
+	}
+	ctx.fs = afero.NewMemMapFs()
+	go processResults()
+	err = json.Unmarshal([]byte(testRunnerResult), &res)
+	if err != nil {
+		t.Fatalf("json.Unmarshal: %v", err)
+	}
+	ctx.Channels.Results <- res
+	extime := time.Now().Add(time.Second * 5)
+	// Verify we have the new result queued up in the inflight directory for the entity
+	for {
+		fd, err := ctx.fs.Open("./testdata/runner/runners/test/inflight/8652929898678.json")
+		if err == nil {
+			defer fd.Close()
+			buf, err := ioutil.ReadAll(fd)
+			if err != nil {
+				t.Fatalf("ioutil.ReadAll: %v", err)
+			}
+			comparebuf, err := json.Marshal(res)
+			if err != nil {
+				t.Fatalf("json.Marshal: %v", err)
+			}
+			if bytes.Compare(buf, comparebuf) != 0 {
+				t.Fatalf("stored inflight action does not equal original")
+			}
+			break
+		}
+		if time.Now().After(extime) {
+			t.Fatalf("inflight action was not queued in time")
+		}
+		time.Sleep(time.Millisecond * 10)
+	}
+}
 
 func TestResultsStoragePath(t *testing.T) {
 	var err error
